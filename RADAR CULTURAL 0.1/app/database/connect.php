68c24ad64cc6d08bb7d297ae6d61ac6e2b756a28
Andre Luis Correa <55238564+Andre-Luis-Correa@users.noreply.github.com>
2022-01-14 19:24:25 -0300

    Add files via upload

---
+++ RADAR CULTURAL 0.1/app/database/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$host = 'localhost';
+$user = 'root';
+$pass = 'andreluis2003';
+$db_name = 'blog_radar_cultural';
+
+$conn = new MySQLi($host, $user, $pass, $db_name);
+
+if ($conn -> connect_error){
+	die('Database connection error: '. $conn -> connect_error);
+}
