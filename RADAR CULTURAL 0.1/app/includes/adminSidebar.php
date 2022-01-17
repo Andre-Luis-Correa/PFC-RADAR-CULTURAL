c1883b67fc55e2d0895bc4e2f874d5dc583e02f3
Andre Luis Correa <55238564+Andre-Luis-Correa@users.noreply.github.com>
2022-01-16 21:54:00 -0300

    Delete RADAR CULTURAL 0.1 directory

--- RADAR CULTURAL 0.1/app/includes/adminSidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="left-sidebar">
-    <ul>
-        <li><a href="<?php echo '../../admin/posts/index.php'; ?>">Manage Posts</a></li>
-        <li><a href="<?php echo '../../admin/users/index.php'; ?>">Manage Users</a></li>
-        <li><a href="<?php echo '../../admin/topics/index.php'; ?>">Manage Topics</a></li>
-    </ul>
-</div>
