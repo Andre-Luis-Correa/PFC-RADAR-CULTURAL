23616ae773d56e731362bbd16f5fa4dafebbb1fb
Andre Luis Correa <55238564+Andre-Luis-Correa@users.noreply.github.com>
2022-01-16 21:56:19 -0300

    Add files via upload

---
+++ RADAR_CULTURAL_0.1/app/controllers/topics.php
@@ -0,0 +1,47 @@
+<?php
+
+include(ROOT_PATH . "/app/database/db.php");
+
+$table = 'tb_categoria';
+
+$id = '';
+$nome = '';
+
+$topics = selectAll($table);
+
+
+if (isset($_POST['add-topic'])) {
+	unset($_POST['add-topic']);
+	$topic_id = create('tb_categoria', $_POST);
+	$_SESSION['message'] = 'Categoria criada com sucesso';
+	$_SESSION['type'] = 'success';
+	header('location: ' . BASE_URL . '/admin/topics/index.php');
+	exit();
+}
+
+if (isset($_GET['id_categoria'])) {
+	$id = $_GET['id_categoria'];
+	$topic = selectOne($table, ['id_categoria' => $id]);
+	$id = $topic['id_categoria'];
+	$nome = $topic['nome'];
+
+}
+
+if (isset($_GET['del_id'])) {
+	$id = $_GET['del_id'];
+	$count = deleteTopic($table, $id);
+	$_SESSION['message'] = 'Categoria deletada com sucesso';
+	$_SESSION['type'] = 'success';
+	header('location: ' . BASE_URL . '/admin/topics/index.php');
+	exit();
+}
+
+if (isset($_POST['update-topic'])) {
+	$id = $_POST['id_categoria'];
+	unset($_POST['update-topic'], $_POST['id_categoria']);
+	$topic_id = updateTopic($table, $id, $_POST);
+
+	$_SESSION['message'] = 'Categoria atualizada com sucesso';
+	header('location: ' . BASE_URL . '/admin/topics/index.php');
+	exit();
+}
---
+++ RADAR_CULTURAL_0.1/app/controllers/users.php
@@ -0,0 +1,85 @@
+<?php
+
+include(ROOT_PATH . "/app/database/db.php");
+include(ROOT_PATH . "/app/helpers/validateUser.php");
+
+$errors = array();
+$nome_completo = '';
+$nome_usuario = '';
+$email = '';
+$senha = '';
+$senhaConf = '';
+$table = 'tb_usuario';
+
+function loginUser($user)
+{
+	$_SESSION['id_usuario'] = $user['id_usuario'];
+
+	$_SESSION['nome_usuario'] = $user['nome_usuario'];
+
+	$_SESSION['tipo_usuario'] = $user['tipo_usuario'];
+
+	$_SESSION['message'] = 'Você está logado';
+
+	$_SESSION['type'] = 'success';
+
+	if ($_SESSION['tipo_usuario'] == 1) {
+			header('location: ' . BASE_URL . '/admin/dashboard.php');	
+	} else{
+			header('location: ' . BASE_URL . '/index.php');
+	}
+
+	exit();
+}
+
+
+if(isset($_POST['register-btn'])){
+
+	$errors = validateUser($_POST);
+
+   if (count($errors) == 0) 
+   {
+   	   unset($_POST['register-btn'], $_POST['senhaConf']);
+	   $_POST['tipo_usuario'] = 0;
+
+	   $_POST['senha'] = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+	   $user_id = create($table, $_POST);
+	   $user = selectOne($table, ['id_usuario' => $user_id]);
+	   
+	   // log user in
+	   loginUser($user);
+
+   } else {
+   		$nome_completo = $_POST['nome_completo'];
+   		$nome_usuario = $_POST['nome_usuario'];
+   		$email = $_POST['email'];
+   		$senha = $_POST['senha'];
+   		$senhaConf = $_POST['senhaConf'];
+   }
+   
+}
+
+//login
+
+if (isset($_POST['login-btn'])) {
+	$errors = validateLogin($_POST);
+
+	if (count($errors) === 0) {
+		$user = selectOne($table, ['nome_usuario' => $_POST['nome_usuario']] );
+
+		if ($user && password_verify($_POST['senha'], $user['senha'])) {
+			
+			loginUser($user);
+
+		} else {
+			array_push($errors, 'Credenciais incorretas');
+		}
+	}
+
+	$nome_usuario = $_POST['nome_usuario'];
+	$senha = $_POST['senha'];
+}
+
+
+
